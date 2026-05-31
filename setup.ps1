38cdb7e5a4987efddc8ded32b6c171fe680aab11
irregularverb <enesatesim+github@gmail.com>
2026-05-31 22:23:22 +0300

    mmmaster

---
+++ setup.ps1
@@ -0,0 +1,10 @@
+Write-Host "Downloading Win11 Full Tweaker..."
+$zip = "$env:TEMP\StarterPack.zip"
+$dir = "$env:TEMP\StarterPack"
+
+# Download and extract the repo locally
+irm "https://github.com/enesatesim/win11-full-tweaker/archive/refs/heads/main.zip" -OutFile $zip
+Expand-Archive $zip -DestinationPath $dir -Force
+
+# Run your actual master script from the extracted local folder
+& "$dir\master.ps1"
