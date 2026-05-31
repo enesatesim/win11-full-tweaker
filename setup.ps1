db9a4a5af521188313266343a39cf202293f53ca
irregularverb <enesatesim+github@gmail.com>
2026-05-31 22:34:21 +0300

    setup

--- setup.ps1
+++ setup.ps1
@@ -7,4 +7,4 @@ irm "https://github.com/enesatesim/win11-full-tweaker/archive/refs/heads/main.zi
 Expand-Archive $zip -DestinationPath $dir -Force
 
 # Run your actual master script from the extracted local folder
-& "$dir\master.ps1"
+& "$dir\win11-full-tweaker-main\master.ps1"
