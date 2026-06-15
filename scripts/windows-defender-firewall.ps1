752ab8252b143bbdf1167db750a675f4f5bf543a
irregularverb <enesatesim+github@gmail.com>
2026-06-15 13:11:12 +0300

    raws

--- scripts/windows-defender-firewall.ps1
+++ scripts/windows-defender-firewall.ps1
@@ -1,8 +1,7 @@
-# Matrix of applications and their paths (Notice the commas at the start of each line)
 # 1. Define the matrix using {0} where you want the program name to appear
 $rawMatrix = @(
     , @("Acrobat"       , "%ProgramFiles%\Adobe\Acrobat DC\Acrobat\{0}.exe")
-    , @("After Effects" , "%ProgramFiles%\Adobe\Adobe {0} 2026\Support Files\AfterFX.exe") # Outlier naming from Adobe
+    , @("After Effects" , "%ProgramFiles%\Adobe\Adobe After Effects 2026\Support Files\AfterFX.exe") # No {0} needed here
     , @("Illustrator"   , "%ProgramFiles%\Adobe\Adobe {0} 2026\Support Files\Contents\Windows\{0}.exe")
     , @("InDesign"      , "%ProgramFiles%\Adobe\Adobe {0} 2026\{0}.exe")
     , @("Photoshop"     , "%ProgramFiles%\Adobe\Adobe {0} 2026\{0}.exe")
@@ -10,8 +9,17 @@ $rawMatrix = @(
     , @("Media Encoder" , "%ProgramFiles%\Adobe\Adobe {0} 2026\Adobe {0}.exe")
 )
 
+# 2. Process the matrix to inject the names into the paths
+$matrix = foreach ($row in $rawMatrix) {
+    $name = $row[0]
+    $path = $row[1] -f $name 
+    
+    # Return the updated pair back to the new $matrix
+    , @($name, $path)
+}
+
 # Loop through each item in the matrix
-foreach ($app in $rawMatrix) {
+foreach ($app in $matrix) {
     $appName = $app[0]
     $appPath = $app[1]
 
