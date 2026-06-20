b82e263724d6dfe3dc613179106940d676f92ef6
irregularverb <enesatesim+github@gmail.com>
2026-06-20 22:55:15 +0300

    new bloat

--- scripts/remove-bloatware.ps1
+++ scripts/remove-bloatware.ps1
@@ -1,7 +1,19 @@
 $bloatware = @(
-    "Microsoft.Todos",
-    "Microsoft.WindowsFeedbackHub",
-    "Clipchamp.Clipchamp"
+    "*Clipchamp.Clipchamp*",
+    "*Microsoft.WindowsFeedbackHub*",
+    "*Microsoft.Todos*",
+    "*Microsoft.PowerAutomateDesktop*",
+    "*Microsoft.MicrosoftSolitaireCollection*",
+    "*Microsoft.MicrosoftOfficeHub*",
+    "*Microsoft.BingNews*",
+    "*Microsoft.BingSearch*",
+    "*Microsoft.MicrosoftStickyNotes*",
+    "*Microsoft.Microsoft.BingWeather*",
+    "*Microsoft.BingWeather*",
+    "*Microsoft.GetHelp*",
+    "*Microsoft.YourPhone*",
+    "*Microsoft.WindowsAlarms*",
+    "*MicrosoftCorporationII.QuickAssist*"
 )
 
 foreach ($app in $bloatware) {
