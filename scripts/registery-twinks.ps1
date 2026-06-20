bd314dcdba4fcf37d0352481cf5010580368551b
irregularverb <enesatesim+github@gmail.com>
2026-06-21 02:31:09 +0300

    main func

--- scripts/registery-twinks.ps1
+++ scripts/registery-twinks.ps1
@@ -27,6 +27,42 @@ function DisableStoreSearchSuggestionsForAllUsers {
     DisableStoreSearchSuggestions $defaultStoreDbPath
 }
 
+# Disables Microsoft Store search suggestions in the start menu by denying access to the Store app database file
+function DisableStoreSearchSuggestions {
+    param (
+        $StoreAppsDatabase = "$env:LocalAppData\Packages\Microsoft.WindowsStore_8wekyb3d8bbwe\LocalState\store.db"
+    )
+
+    # Change path to correct user if a user was specified
+    if ($script:Params.ContainsKey("User")) {
+        $StoreAppsDatabase = GetUserDirectory -userName "$(GetUserName)" -fileName "AppData\Local\Packages\Microsoft.WindowsStore_8wekyb3d8bbwe\LocalState\store.db" -exitIfPathNotFound $false
+    }
+
+    $userName = [regex]::Match($StoreAppsDatabase, '(?:Users\\)([^\\]+)(?:\\AppData)').Groups[1].Value
+
+    # This file doesn't exist in EEA (No Store app suggestions).
+    if (-not (Test-Path -Path $StoreAppsDatabase))
+    {
+        Write-Host "Unable to find Store app database for user $userName, creating it now to prevent Windows from creating it later..." -ForegroundColor Yellow
+
+        $storeDbDir = Split-Path -Path $StoreAppsDatabase -Parent
+
+        if (-not (Test-Path -Path $storeDbDir)) {
+            New-Item -Path $storeDbDir -ItemType Directory -Force | Out-Null
+        }
+
+        New-Item -Path $StoreAppsDatabase -ItemType File -Force | Out-Null
+    }
+    
+    $AccountSid = [System.Security.Principal.SecurityIdentifier]::new('S-1-1-0') # 'EVERYONE' group
+    $Acl = Get-Acl -Path $StoreAppsDatabase
+    $Ace = [System.Security.AccessControl.FileSystemAccessRule]::new($AccountSid, 'FullControl', 'Deny')
+    $Acl.SetAccessRule($Ace) | Out-Null
+    Set-Acl -Path $StoreAppsDatabase -AclObject $Acl | Out-Null
+
+    Write-Host "Disabled Microsoft Store search suggestions for user $userName"
+}
+
 Write-Host "> Disabling Microsoft Store search suggestions in the start menu for all users..."
 DisableStoreSearchSuggestionsForAllUsers
 Write-Host ""
