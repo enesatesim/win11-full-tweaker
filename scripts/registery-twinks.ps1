ea31593e0c4f47936ed6b9e6eed071df574d78e4
irregularverb <enesatesim+github@gmail.com>
2026-06-21 02:26:14 +0300

    store deny

--- scripts/registery-twinks.ps1
+++ scripts/registery-twinks.ps1
@@ -8,3 +8,21 @@ Get-ChildItem -Path $regDir -Filter *.reg -File | Sort-Object Name | ForEach-Obj
 	Write-Output "Importing registry file: $($_.FullName)"
 	reg import $_.FullName
 }
+
+
+
+# Disables Microsoft Store search suggestions in the start menu for all users by denying access to the Store app database file for each user
+function DisableStoreSearchSuggestionsForAllUsers {
+    # Get path to Store app database for all users
+    $userPathString = GetUserDirectory -userName "*" -fileName "AppData\Local\Packages"
+    $usersStoreDbPaths = Get-ChildItem -Path $userPathString -ErrorAction SilentlyContinue
+
+    # Go through all users and disable start search suggestions
+    ForEach ($storeDbPath in $usersStoreDbPaths) {
+        DisableStoreSearchSuggestions ($storeDbPath.FullName + "\Microsoft.WindowsStore_8wekyb3d8bbwe\LocalState\store.db")
+    }
+
+    # Also disable start search suggestions for the default user profile
+    $defaultStoreDbPath = GetUserDirectory -userName "Default" -fileName "AppData\Local\Packages\Microsoft.WindowsStore_8wekyb3d8bbwe\LocalState\store.db" -exitIfPathNotFound $false
+    DisableStoreSearchSuggestions $defaultStoreDbPath
+}
