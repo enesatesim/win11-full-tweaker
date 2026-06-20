555a99d0d77c1da433c90108c97d0f1d1ea06853
irregularverb <enesatesim+github@gmail.com>
2026-06-21 02:52:39 +0300

    blody commit

--- scripts/registry-tweaks.ps1
+++ scripts/registry-tweaks.ps1
@@ -1,12 +1,10 @@
-# Show file extensions in Explorer
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
+$regDir = Join-Path -Path $PSScriptRoot -ChildPath "..\reg-files"
+if (-not (Test-Path $regDir)) {
+	Write-Error "Reg files directory not found: $regDir"
+	exit 1
+}
 
-# Align the Start Menu to the Left (Windows 10 style)
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 0
-
-# Hide the annoying "Search Highlights" in the search box
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 1
-
-# Enable Dark Mode (System and Apps)
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+Get-ChildItem -Path $regDir -Filter *.reg -File | Sort-Object Name | ForEach-Object {
+	Write-Output "Importing registry file: $($_.FullName)"
+	reg import $_.FullName
+}
