216a7987ff0388cc3db97e54755f165b6bfe3caf
irregularverb <enesatesim+github@gmail.com>
2026-06-21 18:02:39 +0300

    changes

--- master.ps1
+++ master.ps1
@@ -14,12 +14,12 @@ $scriptsDir = "$rootDir\scripts"
 # Execute the local child scripts
 # & "$scriptsDir\remove-bloatware.ps1"
 & "$scriptsDir\registry-tweaks.ps1"
-# & "$scriptsDir\set-desktop-wallpaper.ps1"
+& "$scriptsDir\set-desktop-wallpaper.ps1"
 # & "$scriptsDir\windows-defender-firewall.ps1"
 # & "$scriptsDir\install-apps.ps1"
 
 # Restart Windows Explorer to apply changes
-# Stop-Process -Name explorer -Force
+Write-Host "Restarting Windows Explorer to apply changes..." -ForegroundColor Yellow
+Stop-Process -Name explorer -Force
 
 Write-Host "Setup completed successfully!" -ForegroundColor Green
-pause
