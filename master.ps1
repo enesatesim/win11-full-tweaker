1f175ab6426868600b22682192e9bb6583e428fe
irregularverb <enesatesim+github@gmail.com>
2026-06-21 23:43:05 +0300

    new

--- master.ps1
+++ master.ps1
@@ -12,7 +12,7 @@ $rootDir = $PSScriptRoot
 $scriptsDir = "$rootDir\scripts"
 
 # Execute the local child scripts
-# & "$scriptsDir\remove-bloatware.ps1"
+& "$scriptsDir\remove-bloatware.ps1"
 & "$scriptsDir\registry-tweaks.ps1"
 & "$scriptsDir\set-desktop-wallpaper.ps1"
 # & "$scriptsDir\windows-defender-firewall.ps1"
