9a57b387642ca8ada0bbf8ebe4e9cdd68162a914
irregularverb <enesatesim+github@gmail.com>
2026-06-21 17:39:55 +0300

    better install apps

--- scripts/install-apps.ps1
+++ scripts/install-apps.ps1
@@ -6,13 +6,29 @@ $favApps = @(
     "AutoHotkey.AutoHotkey"
 )
 
-foreach ($app in $favApps) {
-    Write-Host "Installing $app..."
-    winget.exe install --id "$app" --exact `
-        --source winget `
-        --accept-source-agreements `
-        --disable-interactivity `
-        --silent `
-        --accept-package-agreements `
-        --force
+Write-Host "Queueing applications for installation..." -ForegroundColor Cyan
+
+# Start all installations simultaneously as background jobs
+$jobs = foreach ($app in $favApps) {
+    Start-Job -ScriptBlock {
+        param($appName)
+        winget.exe install --id "$appName" --exact `
+            --source winget `
+            --accept-source-agreements `
+            --disable-interactivity `
+            --silent `
+            --accept-package-agreements `
+            --force
+    } -ArgumentList $app
 }
+
+# Wait for all jobs to complete 
+Wait-Job -Job $jobs | Out-Null
+
+# Output the results of the jobs to the console
+Receive-Job -Job $jobs
+
+# Clean up the jobs from memory
+Remove-Job -Job $jobs
+
+Write-Host "All applications processed!" -ForegroundColor Green
