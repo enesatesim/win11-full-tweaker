6237aa1c2ccfd373cf8cb1810a6fb6865074acd0
irregularverb <enesatesim+github@gmail.com>
2026-06-27 15:41:31 +0300

    seperated the function

--- scripts/install-apps.ps1
+++ scripts/install-apps.ps1
@@ -1,24 +1,3 @@
-function Install-AppList {
-    param(
-        [string[]]$Apps,
-        [string]$Source = "winget"
-    )
-
-    foreach ($app in $Apps) {
-        Write-Host "Installing $app..." -ForegroundColor Green
-
-        winget.exe install `
-            --id "$app" `
-            --exact `
-            --source $Source `
-            --accept-source-agreements `
-            --disable-interactivity `
-            --silent `
-            --accept-package-agreements `
-            --force
-    }
-}
-
 $favWingetApps = @(
     "Brave.Brave",
     "M2Team.NanaZip",
@@ -36,5 +15,30 @@ $favMSStoreApps = @(
     "9PFXXSHC64H3"
 )
 
-Install-AppList -Apps $favMSStoreApps -Source "msstore"
-Install-AppList -Apps $favWingetApps -Source "winget"
+foreach ($app in $favWingetApps) {
+    Write-Host "Installing $app..." -ForegroundColor Green
+
+    winget.exe install `
+        --id "$app" `
+        --exact `
+        --source winget `
+        --accept-source-agreements `
+        --disable-interactivity `
+        --silent `
+        --accept-package-agreements `
+        --force
+}
+
+foreach ($app in $favMSStoreApps) {
+    Write-Host "Installing $app..." -ForegroundColor Green
+
+    winget.exe install `
+        --id "$app" `
+        --exact `
+        --source winget `
+        --accept-source-agreements `
+        --disable-interactivity `
+        --silent `
+        --accept-package-agreements `
+        --force
+}
